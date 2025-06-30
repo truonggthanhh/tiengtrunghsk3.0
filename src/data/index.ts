53421dfd07254cf69fba4a7f1a7d77c83021e03a
[dyad] <git@dyad.sh>
2025-06-30 07:58:23 +0700

    [dyad] Cập nhật từ vựng HSK 4 - wrote 2 file(s)

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,12 +1,13 @@
 import { hsk1Vocabulary, type VocabularyWord } from './hsk1';
 import { hsk2Vocabulary } from './hsk2';
 import { hsk3Vocabulary } from './hsk3';
+import { hsk4Vocabulary } from './hsk4';
 
 const vocabularyData: { [key: string]: VocabularyWord[] } = {
   '1': hsk1Vocabulary,
   '2': hsk2Vocabulary,
   '3': hsk3Vocabulary,
-  '4': [],
+  '4': hsk4Vocabulary,
   '5': [],
   '6': [],
 };
