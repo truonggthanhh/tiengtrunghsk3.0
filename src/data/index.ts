dc1783bb3c4d078b16df314c0478d52bf9576c88
[dyad] <git@dyad.sh>
2025-06-30 08:12:00 +0700

    [dyad] Cập nhật từ vựng HSK 5 - wrote 2 file(s)

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,13 +2,14 @@ import { hsk1Vocabulary, type VocabularyWord } from './hsk1';
 import { hsk2Vocabulary } from './hsk2';
 import { hsk3Vocabulary } from './hsk3';
 import { hsk4Vocabulary } from './hsk4';
+import { hsk5Vocabulary } from './hsk5';
 
 const vocabularyData: { [key: string]: VocabularyWord[] } = {
   '1': hsk1Vocabulary,
   '2': hsk2Vocabulary,
   '3': hsk3Vocabulary,
   '4': hsk4Vocabulary,
-  '5': [],
+  '5': hsk5Vocabulary,
   '6': [],
 };
 
