ac5fbd11372901908ea6e604992711a06581abc1
[dyad] <disneychanelmovie@gmail.com>
2025-07-02 21:34:51 +0700

    [dyad] Fixing an import error - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import SentenceScramblePage from "./pages/SentenceScramblePage";
 import PronunciationPage from "./pages/PronunciationPage";
 import AiTutorPage from "./pages/AiTutorPage";
 import MsutongPage from "./pages/MsutongPage";
-import MsutongPracticePage from "./pages/msutong/MsutongPracticePage";
 import MsutongAiTutorPage from "./pages/msutong/AiTutorPage";
 import MsutongSentenceChoicePage from "./pages/msutong/SentenceChoicePage";
 import MsutongSentenceScramblePage from "./pages/msutong/SentenceScramblePage";
@@ -44,7 +43,6 @@ const App = () => (
 
             {/* Msutong Routes */}
             <Route path="/msutong" element={<MsutongPage />} />
-            <Route path="/msutong/practice" element={<MsutongPracticePage />} />
             <Route path="/msutong/sentence-choice" element={<MsutongSentenceChoicePage />} />
             <Route path="/msutong/sentence-scramble" element={<MsutongSentenceScramblePage />} />
             <Route path="/msutong/ai-tutor" element={<MsutongAiTutorPage />} />
